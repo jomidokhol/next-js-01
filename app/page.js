5d1e7befdf25c8ae1bc9ab15a12010596fba85ca
jomidokhol <grownm.yt@gmail.com>
2026-04-09 01:09:54 +0600

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+      <h1>হ্যালো ওয়ার্ল্ড! 👋</h1>
+      <p>এটি আমার জীবনের প্রথম Next.js প্রজেক্ট, যা আমি মোবাইল দিয়ে বানিয়েছি!</p>
+    </div>
+  )
+}
